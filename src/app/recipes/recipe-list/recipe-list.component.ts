60036bbccf0263df1bd265b9353c1050c175126c
Marejean Perpinosa <marejean.perpinosa@ibm.com>
2022-05-06 16:16:01 +0800

    Shopping, recipes components

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test.', 'https://www.saveur.com/uploads/2020/11/20/Y7RZPFZEERAZVHJ2VHC2RXMEEY.jpg?quality=85&width=540'), 
+    new Recipe('A Test Recipe', 'This is simply a test.', 'https://www.saveur.com/uploads/2020/11/20/Y7RZPFZEERAZVHJ2VHC2RXMEEY.jpg?quality=85&width=540') 
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
